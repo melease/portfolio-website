8afbc84f29292ff2ca4e4bab2a582021e3aca3df
melease <mslpink84@gmail.com>
2021-04-02 14:52:32 -0700

    navigation work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hiiiiieee")
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
